b7946bef2d1e8b5efbf3e46e07b8ba833422c369
RachamallaSwathi <swathirachamalla77@gmail.com>
2022-04-05 11:09:12 +0530

    github-battle

---
+++ github-battle/src/App.js
@@ -0,0 +1,14 @@
+import "./stylesheet/style.css";
+import Header from './components/Header';
+import GitHub from './components/GitHub';
+
+function App() {
+  return (
+        <>
+          <Header/>
+          <GitHub/>
+        </>
+  );
+}
+
+export default App;
